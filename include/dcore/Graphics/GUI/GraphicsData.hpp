32627b406e83761892cd9b6ac27329d34aa3f1bc
Mike Redko <30830926+somerandomdev49@users.noreply.github.com>
2021-10-28 14:01:51 +0300

    gui ready for idakrn to take over

--- include/dcore/Graphics/GUI/GraphicsData.hpp
+++ include/dcore/Graphics/GUI/GraphicsData.hpp
@@ -4,12 +4,15 @@
 
 namespace dcore::graphics::gui
 {
-	struct Quad
+	struct Quad final
 	{
 		glm::vec2 Position;
 		glm::vec2 Scale;
 		float Rotation;
 		RTexture *Texture;
 		glm::vec4 Color;
+
+		bool OverlapPoint(const glm::vec2 &pointer);
+		bool OverlapQuad(const Quad &other);
 	};
 } // namespace dcore::graphics::gui
