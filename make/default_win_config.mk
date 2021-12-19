f8a368ecbf857a2f710359ed79601cc403b0686d
somerandomdev49 <mike.redko49@gmail.com>
2021-12-19 17:53:04 +0300

    wowee

--- make/default_win_config.mk
+++ make/default_win_config.mk
@@ -1,15 +1,15 @@
 #### PROJECT SETTINGS FOR WINDOWS (MinGW32-w64) ####
 BIN_NAME := dcore
-CXX = g++-10
+CXX = g++
 SRC_EXT = cpp
 SRC_PATH = src
-LIBS = freetype2 nlohmann_json # pkg-config files
-COMPILE_FLAGS = -std=c++17 -Wall -Wextra -DLOGURU_WITH_STREAMS -rdynamic -g
+LIBS = freetype2 nlohmann_json
+COMPILE_FLAGS = -std=c++17 -Wall -Wextra -DLOGURU_WITH_STREAMS -g
 RCOMPILE_FLAGS = -D NDEBUG
 DCOMPILE_FLAGS = -D DEBUG -rdynamic -g
 INCLUDES = -Iinclude -I3rd-party/loguru -I3rd-party/include
-LINK_FLAGS = -lglfw -ldl -lpthread -std=c++17 -rdynamic -g
+LINK_FLAGS = -lglfw -ldl -lpthread -std=c++17 -g
 RLINK_FLAGS = 
-DLINK_FLAGS = -rdynamic -g
+DLINK_FLAGS = -g
 DESTDIR = /
 INSTALL_PREFIX = usr/local
